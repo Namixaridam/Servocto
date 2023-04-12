b7980abb90c49e7ed78e89d53c5125fed01c541d
UnstressedSchwa <128149300+Namixaridam@users.noreply.github.com>
2023-04-12 15:23:57 +0300

    Update servocto.h

--- src/servocto.h
+++ src/servocto.h
@@ -16,6 +16,8 @@ class servocto
     void end();
     //no voltage output
     bool noOutput(bool state);
+    //inverts a signal
+    void invert(bool inverting);
     //write in degrees
     void writeAngle(uint8_t servo, uint8_t angle);
     //write in microseconds
